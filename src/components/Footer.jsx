72acfba420335b13581ee505d9f25e34bf355025
Adeolu Alagbe <alagbeade@gmail.com>
2023-08-24 17:20:39 +0100

    Styling for the eader and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
+import AppLogo from "./AppLogo";
+import Payment from "../assets/img/payment.png"
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <footer className="">
+      <section>
+        <div>
+          <h1>Categories</h1>
+          <span>Men</span>
+          <span>Women</span>
+          <span>Shoes</span>
+          <span>Accesories</span>
+        </div>
+        <div>
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+        </div>
+        <div>
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            culpa. Rerum repellat, illum id dolorem sed vitae labore incidunt
+            maiores harum tempore? Suscipit sit consequuntur exercitationem
+            dolore dolorum molestias eos.
+          </span>
+        </div>
+
+        <div>
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            culpa. Rerum repellat, illum id dolorem sed vitae labore incidunt
+            maiores harum tempore? Suscipit sit consequuntur exercitationem
+            dolore dolorum molestias eos.
+          </span>
+        </div>
+      </section>
+      <section>
+        <div>
+          <span><AppLogo /></span>
+          <span>Copyright 2023. All Rights Reserved. Inspired by LAMADEV</span>
+        </div>
+        <div>
+          <img src={Payment} alt="" className="w-[100px]"/>
+        </div>
+      </section>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,54 @@
 import React from "react";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import { PersonOutlineOutlined } from "@mui/icons-material"
+import { PersonOutlineOutlined } from "@mui/icons-material";
 import { Link } from "react-router-dom";
-import AppLogo from './AppLogo'
+import AppLogo from "./AppLogo";
 
 function Navbar() {
+  const item = "flex items-center text-[18px]";
+
   return (
-    <div>
-      <div className="left"></div>
-      <div className="center">
-        <Link to="/">
-          <span className="flex items-center">
-            <AppLogo />Apparelista
+    <div className="h-[80px]">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-[25px] ">
+          <div className={item}>
+            <Link to="/">Men</Link>
+          </div>
+          <div className={item}>
+            <Link to="/">Women</Link>
+          </div>
+          <div className={item}>
+            <Link to="/">Children</Link>
+          </div>
+        </div>
+        <div className="text-[30px] tracking-[2px]">
+          <Link to="/">
+            <span className="flex items-center">
+              <AppLogo />
+              Apparelista
             </span>
-        </Link>
-      </div>
-      <div className="right">
-        <Link to='/' ></Link>
-        <Link to='/' ></Link>
-        <Link to='/' ></Link>
-        <Link to='/' ></Link>
-        <ShoppingCartOutlinedIcon />
-        <SearchOutlinedIcon />
-        <PersonOutlineOutlined />
+          </Link>
+        </div>
+        <div className="flex items-center gap-[25px]">
+          <div className={item}>
+            <Link to="/">Homepage</Link>
+          </div>
+          <div className={item}>
+            <Link to="/">About</Link>
+          </div>
+          <div className={item}>
+            <Link to="/">Store</Link>
+          </div>
+          <div className="flex gap-[15px] cursor-pointer text-[#777]">
+            <SearchOutlinedIcon />
+            <PersonOutlineOutlined />
+            <div className="carticon relative">
+              <ShoppingCartOutlinedIcon />
+              <span className="w-[20px] h-[20px] text-[12px] rounded-[50%] items-center flex justify-center absolute right-[-10px] top-[-10px] text-white bg-[#2879fe]">0</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
