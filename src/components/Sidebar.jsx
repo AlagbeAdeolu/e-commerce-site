41fc73858d790d46f190c76e1012404572830efc
Adeolu Alagbe <alagbeade@gmail.com>
2023-08-26 20:07:09 +0100

    Added the sidebar context and styles

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { ArrowForward, DeleteOutline } from '@mui/icons-material'
+
+const Sidebar = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Sidebar
